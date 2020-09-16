74026f35bdc6fea5cfafed6b0f7722d4f88b3d0f
Alexey_Vitvor <vitvor01@gmail.com>
2020-09-16 20:24:22 +0300

    Update date

--- Model/ResultModels/Result.cs
+++ Model/ResultModels/Result.cs
@@ -1,13 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.ComponentModel.DataAnnotations;
 
 namespace DB.ResultModels
 {
-    
+
     public class Result
     {
         public int Id { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM'/'dd'/'yyyy}", ApplyFormatInEditMode = true)]
         public DateTime Date { get; set; }
         public string Name { get; set; }
         public float Hours { get; set; }
